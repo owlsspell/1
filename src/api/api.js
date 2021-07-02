9603b2fbea160144a496c91145cd1c50a330694c
edev@evernetica.com <!]RB!b%,yRC,'F7>
2021-07-02 10:59:21 +0300

    reset main

--- src/api/api.js
+++
@@ -1,9 +0,0 @@
-import * as axios from "axios";
-
-const instanse = axios.create({
-  baseURL: "https://restcountries.eu/rest/v2/",
-});
-
-export const getCountries = () => {
-  return instanse.get("all").then((response) => response.data);
-};
--- src/redux/redux-store.js
+++
@@ -1,8 +0,0 @@
-import cardReduser from "./card-reduser";
-const { combineReducers, createStore } = require("redux");
-
-const redusers = combineReducers({ cardReduser: cardReduser });
-
-const store = createStore(redusers);
-
-export default store;
